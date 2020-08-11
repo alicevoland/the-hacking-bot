92dd68d50d24b55d167337e114ec5c3276262cb6
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-11 13:55:38 +0200

    user view and profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
+
+  resources :users, only: [:show]
+
+  get 'profile', to: 'users#profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
