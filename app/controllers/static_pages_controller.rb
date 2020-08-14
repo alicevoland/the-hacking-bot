769b037564223efa5dfc6e07d99e7657690779d1
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-15 00:17:56 +0200

    put back jumbotron on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  layout 'layouts/home'
   def home
     flash.now[:notice] = 'Tu viens de réveiller The Hacking Bot, merci !' if DiscordBot.keep_alive
     @page_title = 'The Hacking Bot | Accueil'
