03b73eee813f9b2c4b5f305243839125fc724e0f
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-14 23:54:31 +0200

    Refactored views and layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[profile discord_verify]
+  layout 'layouts/users'
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def profile
-    @user = current_user
-  end
-
-  def discord_verify
-    @user = current_user
-    @discord_verify_token = SecureRandom.base64(10)
-    @user.update(discord_verify_digest: BCrypt::Password.create(@discord_verify_token))
+    if user_signed_in? && current_user == @user
+      unless @user.discord?
+        flash.now[:danger] = "Merci de lier le compte discord voici la commande à envoyer au bot : $verify #{@user.email} #{@user.create_discord_verify_token}"
+      end
+      render :profile
+    else
+      render :show
+    end
   end
 
   def index
